e6d9d39d2944afe972ec8463d228762902ebb99d
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-07-05 12:24:54 -0600

    Adding types and constants. Also was  modifying layout and page to add <Hero/>, <Footer/> & <Navbar/> components. Finally was modified globals.css file and were added some images for the project

---
+++ components/CustomButton.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
+
+export default function CustomButton({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) {
+  return (
+    <button
+      disabled={false}
+      type={btnType}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 `}>{title}</span>
+    </button>
+  );
+}
