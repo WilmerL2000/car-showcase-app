6d02046dcfeb49840eae259f4ef6900c587acebf
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-07-05 22:37:43 -0600

    Modified Home page, also types and utils files to add more interfaces and new util functions, <SearchBar/> & <CustomButton/> component. Were added the code of <CustomFilter/> & <CarCard/> components. Were created <CarDetails/> & <ShowMore/>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components';
 import { fetchCars } from '@/utils';
+import { HomeProps } from '@/types';
+import { fuels, yearsOfProduction } from '@/constants';
+
 import Image from 'next/image';
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -18,8 +27,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
@@ -30,6 +39,11 @@ export default async function Home() {
                 <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,20 @@
 import { MouseEventHandler } from "react";
 
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
 export interface CustomButtonProps {
     isDisabled?: boolean;
     btnType?: "button" | "submit";
@@ -16,6 +31,16 @@ export interface FilterProps {
     model?: string;
     limit?: number;
     fuel?: string;
+}
+
+export interface CarCardProps {
+    model: string;
+    make: string;
+    mpg: number;
+    transmission: string;
+    year: number;
+    drive: string;
+    cityMPG: number;
 }
 
 export interface HomeProps {
